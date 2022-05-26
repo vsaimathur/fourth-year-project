ea68107b9a8b9d2d78d2f615ee6ccd61c48c1352
Veeravajhula Sai Mathur <51848442+vsaimathur@users.noreply.github.com>
2022-05-26 22:00:26 +0530

    Update number_guess.py

--- number_guess.py
+++ number_guess.py
@@ -1,26 +1,29 @@
-import random
+from random import randint
 import math
 
 lower = 3
 upper = 20
 tries = 10
 
-x = random.randint(lower, upper)
-print("\n\tYou've only ", tries, " chances to guess the integer!\n")
-
+x = randint(lower, upper)
+print('\n\tYou\'ve only ',
+       tries,
+      ' chances to guess the integer!\n')
+ 
 # for calculation of minimum number of
 # guesses depends upon range
 while tries != 0:
     tries -= 1
     # taking guessing number as input
-    guess = int(input("Guess a number:- "))
-
+    guess = int(input('Guess a number:- '))
+ 
     # Condition testing
     if x == guess:
-        print("Congratulations you did it in ", count, " try")
+        print('Congratulations you did it in ',
+              count, ' try')
         # Once guessed, loop will break
         break
     elif x > guess:
-        print("You guessed too small!")
+        print('You guessed too small!')
     elif x < guess:
-        print("You Guessed too high!")
+        print('You Guessed too high!')
